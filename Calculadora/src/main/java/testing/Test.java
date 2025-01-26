5d1820f19238c3587d9bb55a3a68cf482412d111
alberto-v-g <alberto.velasco376@comunidadunir.net>
2025-01-26 19:29:13 +0100

    clase division

--- Calculadora/src/main/java/testing/Test.java
+++ Calculadora/src/main/java/testing/Test.java
@@ -1,7 +1,10 @@
 package testing;
 
+import javabean.Cociente;
+
 public class Test {
     public static void main(String[] args) {
-        
+        Cociente cociente1 = new Cociente();
+        System.out.println(cociente1.calcular(10,2));
     }
 }
