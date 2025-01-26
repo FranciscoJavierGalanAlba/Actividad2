f67cc9c383fa83320d4201c046cd2d898efe6e33
FranciscoJavierGalanAlba <galanalbafranciscojavier@gmail.com>
2025-01-26 18:50:55 +0100

    primer commit

---
+++ prueba/src/main/java/javabean/Suma.java
@@ -0,0 +1,12 @@
+package javabean;
+
+public class Suma {
+    private int num1;
+    private int num2;
+
+    public void Suma(int num1, int num2){
+        if(num1>=0 && num2>=0){
+            System.out.println(num1 + num2);
+        }
+    }
+}
