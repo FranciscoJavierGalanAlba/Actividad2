f24a7c529df300f07107e0899af1d5541a5b361b
FranciscoJavierGalanAlba <galanalbafranciscojavier@gmail.com>
2025-01-26 22:02:32 +0100

    incorporando comentarios a la clase suma

--- Calculadora/src/main/java/javabean/Suma.java
+++ Calculadora/src/main/java/javabean/Suma.java
@@ -1,12 +1,38 @@
 package javabean;
 
-public class Suma {
-    private int num1;
-    private int num2;
+/**
+ *  Clase cuya función es la de sumar dos numeros
+ * @autor Francisco Javier Galan Alba (FranciscoJavierGalanAlba)
+ * @version 0.1
+ */
 
-    public void Suma(int num1, int num2){
+public class Suma {
+    private Double suma;
+    /**
+     * Método cuya función es recibir dos numeros y mostrar por pantalla
+     * la suma de ambos.
+     *
+     * @param num1 es el primer numero que se recibe
+     * @param num2 es el segundo numero que se recibe
+     *
+     */
+    public Double sumando(double num1, double num2){
+
+        /**
+         * Creo un condicional.
+         *
+         * Si ambos numeros son positvos, el metodo devuelve la suma de ambos
+         *
+         * Si uno o ambos de los numeros es negativo, me devuelve null
+         */
         if(num1>=0 && num2>=0){
-            System.out.println(num1 + num2);
+            suma = (num1 + num2);
+            System.out.println("La suma de los numeros: " + num1 +
+                    " y " + num2 + " es: " );
+            return (num1 + num2);
+        } else{
+            return null;
         }
+        
     }
 }
--- Calculadora/src/main/java/testing/Test.java
+++ Calculadora/src/main/java/testing/Test.java
@@ -1,7 +1,10 @@
 package testing;
 
+import javabean.Suma;
+
 public class Test {
     public static void main(String[] args) {
-        
+        Suma suma = new Suma();
+        System.out.println(suma.suma(1, 1));
     }
 }
