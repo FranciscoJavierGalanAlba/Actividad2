d1dad1cefedef4f8773c0571da6edcbaab2fec35
FranciscoJavierGalanAlba <galanalbafranciscojavier@gmail.com>
2025-02-02 20:55:02 +0100

    incorporando dependencia junit y modificando la clase Suma

--- Calculadora/src/main/java/javabean/Suma.java
+++ Calculadora/src/main/java/javabean/Suma.java
@@ -1,38 +1,84 @@
 package javabean;
 
+import java.util.Scanner;
+
 /**
- *  Clase cuya función es la de sumar dos numeros
- * @autor Francisco Javier Galan Alba (FranciscoJavierGalanAlba)
- * @version 0.1
+ *  Clase cuya función es la de sumar numeros, ya sean enteros o
+ *  reales
+ * @author Francisco Javier Galan Alba (FranciscoJavierGalanAlba)
+ * @version 0.2 segunda version
  */
 
 public class Suma {
     private Double suma;
+
     /**
-     * Método cuya función es recibir dos numeros y mostrar por pantalla
-     * la suma de ambos.
+     * Metodo cuya funcion es sumar dos numeros reales
+     * Recibe dos numeros, el programa realiza la suma y devuelve
+     * la suma de ambos numeros.
      *
      * @param num1 es el primer numero que se recibe
      * @param num2 es el segundo numero que se recibe
      *
+     * @return Devuelve la suma de los dos numeros.
      */
-    public Double sumando(double num1, double num2){
 
-        /**
-         * Creo un condicional.
-         *
-         * Si ambos numeros son positvos, el metodo devuelve la suma de ambos
-         *
-         * Si uno o ambos de los numeros es negativo, me devuelve null
-         */
+    public Double sumaRealesDos(double num1, double num2){
+
+        Double suma = num1 + num2;
+
+        System.out.println("La suma de los numeros: " + num1 +
+                " y " + num2 + " es: "  );
+
+        return suma;
+    }
+
+    /**
+     * Recice dos numeros enteros, el programa realiza la suma y
+     * devuelve dicha suma
+     * El programa realiza una condicion, si ambos numeros son
+     * positivos devuelve la suma de ambos numeros.
+     * Si al menos uno de los numeros o los dos son negativos,
+     * devuelve un null.
+     *
+     * @param num1 es el primer numero que se recibe
+     * @param num2 es el segundo numero que se recibe
+     *
+     * @return devuelve la suma de dos numeros enteros
+     */
+
+    public int sumaEnteros(int num1, int num2){
         if(num1>=0 && num2>=0){
-            suma = (num1 + num2);
+            int suma = num1 + num2;
+
             System.out.println("La suma de los numeros: " + num1 +
                     " y " + num2 + " es: " );
-            return (num1 + num2);
+
+            return suma;
+
         } else{
-            return null;
+            System.out.println("Introduce numeros positivos");
+            return -1;
         }
-        
+
+    }
+
+    /**
+     * Se recibe tres numeros reales. El programa realiza la suma
+     * y devuelve la suma de los numeros.
+     *
+     * @param num1 es el primer numero que se recibe
+     * @param num2 es el segundo numero que se recibe
+     * @param num3 es el tercer numero que se recibe
+     *
+     * @return devuleve la suma de tres numeros reales
+     */
+    public Double sumaRealesTres(double num1, double num2, double num3){
+        Double suma = num1 + num2 + num3;
+
+        System.out.println("La suma de los numeros: " + num1 +
+                " + " + num2 + " + " + num3 + " es: " );
+
+        return suma;
     }
 }
--- Calculadora/src/main/java/testing/Test.java
+++ Calculadora/src/main/java/testing/Test.java
@@ -2,9 +2,83 @@ package testing;
 
 import javabean.Suma;
 
+import java.util.Scanner;
+
+/**
+ * Clase cuya función es probrar las clases de suma, resta,
+ * multiplicacion y division
+ *
+ */
 public class Test {
+
+    static Suma suma = new Suma();
+
+    static Scanner scanner = new Scanner(System.in);
+
+    /**
+     * metodo cuya funcion es la de correr el progama
+     * @param args
+     */
     public static void main(String[] args) {
-        Suma suma = new Suma();
-        System.out.println(suma.suma(1, 1));
+        int opcion = 0;
+
+        System.out.println("Bienvenido a mi menu. ¿Que opcion quieres?:");
+        System.out.println("Opcion 1. Suma");
+        System.out.println("Opcion 2. Resta");
+        System.out.println("Opcion 3. Producto");
+        System.out.println("Opcion 4. Cociente");
+
+        opcion = scanner.nextInt();
+
+        do{
+            switch (opcion){
+                case 1:
+                    probandoSumaEntero();
+                    probandoSumaReal();
+                    probandoSumaRealTres();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+
+        }while (opcion!=4);
+
+
+
     }
+
+
+    private static void probandoSumaEntero(){
+        System.out.println("Introduce el primer numero entero: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Introduce el segundo numero entero: ");
+        int num2 = scanner.nextInt();
+        suma.sumaEnteros(num1, num2);
+    }
+
+    private static void probandoSumaReal(){
+        System.out.println("Introduce el primer numero real: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Introduce el segundo numero real: ");
+        double num2 = scanner.nextDouble();
+        suma.sumaRealesDos(num1, num2);
+    }
+
+    private static void probandoSumaRealTres(){
+        System.out.println("Introduce el primer numero real: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Introduce el segundo numero real: ");
+        double num2 = scanner.nextDouble();
+        System.out.println("Introduce el tercer numero real: ");
+        double num3 = scanner.nextDouble();
+
+        suma.sumaRealesTres(num1, num2, num3);
+    }
+
+
+
 }
